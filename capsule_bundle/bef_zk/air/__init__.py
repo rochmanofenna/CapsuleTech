cde287798c92c9feaec413254a99c9b7cdb0a818
Ryan Rochmanofenna <rochmanofenna@gmail.com>
2025-12-18 19:24:51 -0500

    remove legacy artifacts

--- capsule_bundle/bef_zk/air/__init__.py
+++
@@ -1,9 +0,0 @@
-"""AIR definitions used by the BEF ZK stack."""
-
-from . import geom_air, geom_constraints, geom_trace_export
-
-__all__ = [
-    "geom_air",
-    "geom_constraints",
-    "geom_trace_export",
-]
